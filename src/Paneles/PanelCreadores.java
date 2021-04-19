e566d6761983938fa2c2ddab4b5a5e1f369bb1cf
DarianMD <73123059+DarianMD@users.noreply.github.com>
2021-04-19 20:05:24 +0200

    Add files via upload

---
+++ src/Paneles/PanelCreadores.java
@@ -0,0 +1,12 @@
+package Paneles;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+public class PanelCreadores extends JPanel {
+	
+	private ImageIcon imagenFondo = new ImageIcon("./src/Fondos/FondoMenuEquipos.png");
+	
+	
+
+}
